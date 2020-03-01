e1df6879b8ba3e566e94bfd5f5016c70e2617e4a
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-03-01 17:21:13 +0100

    Added basic properties to Producer class

--- Warehouse_Manage_WPF/Entities/Producer.cs
+++ Warehouse_Manage_WPF/Entities/Producer.cs
@@ -8,6 +8,10 @@ namespace Warehouse_Manage_WPF.Entities
 {
     public class Producer
     {
+        public int Id { get; set; }
 
+        public string Name { get; set; }
+
+        public string URL { get; set; }
     }
 }
