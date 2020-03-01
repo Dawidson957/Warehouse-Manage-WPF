ce78860484fb5cb2f34d52b75e8f844865755611
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-03-01 17:20:28 +0100

    Added basic properties to Customer class

--- Warehouse_Manage_WPF/Entities/Customer.cs
+++ Warehouse_Manage_WPF/Entities/Customer.cs
@@ -8,6 +8,12 @@ namespace Warehouse_Manage_WPF.Entities
 {
     public class Customer
     {
+        public int Id { get; set; }
 
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
     }
 }
