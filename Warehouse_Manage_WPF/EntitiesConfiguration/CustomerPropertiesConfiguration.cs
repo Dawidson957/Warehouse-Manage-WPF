4262d7e5828500bc3913944d26b22ffa683f9554
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-03-01 16:57:38 +0100

    Added EntitiesConfiguration directory with empty classes for configuration

---
+++ Warehouse_Manage_WPF/EntitiesConfiguration/CustomerPropertiesConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+using Warehouse_Manage_WPF.Entities;
+
+namespace Warehouse_Manage_WPF.EntitiesConfiguration
+{
+    public class CustomerPropertiesConfiguration : EntityTypeConfiguration<Customer>
+    {
+        public CustomerPropertiesConfiguration()
+        {
+
+        }
+    }
+}
