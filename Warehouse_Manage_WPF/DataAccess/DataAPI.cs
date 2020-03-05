28a1cc1de7181818d5434180e96832334e90fd4f
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-03-05 18:04:14 +0100

    Added DataAcces API to project

---
+++ Warehouse_Manage_WPF/DataAccess/DataAPI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse_Manage_WPF.Entities;
+using Warehouse_Manage_WPF.EntityModel;
+
+namespace Warehouse_Manage_WPF.DataAccess
+{
+    public class DataAPI
+    {
+        private WarehouseModel context { get; set; }
+
+        public DataAPI()
+        {
+            this.context = new WarehouseModel();
+        }
+    }
+}
