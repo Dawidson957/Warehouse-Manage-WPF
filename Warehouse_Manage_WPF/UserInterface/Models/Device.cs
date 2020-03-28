aa7147cf7f470dbf53efaa11b28ad61d0e08db9f
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-03-28 10:31:06 +0100

    Added four Models to UserInterface on the base of DataAccess Entities.

---
+++ Warehouse_Manage_WPF/UserInterface/Models/Device.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_Manage_WPF.UserInterface.Models
+{
+    public class Device
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ArticleNumber { get; set; }
+
+        public string Location { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int ProducerID { get; set; }
+
+        public Producer ProducerObject { get; set; }
+
+        public ICollection<Project> Projects { get; set; }
+
+    }
+}
---
+++ Warehouse_Manage_WPF/UserInterface/Models/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_Manage_WPF.UserInterface.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int CustomerID { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public ICollection<Device> Devices { get; set; }
+
+    }
+}
