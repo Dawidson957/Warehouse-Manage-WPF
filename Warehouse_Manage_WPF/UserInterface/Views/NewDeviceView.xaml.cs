018bc126e9a3fb16c4d18142709d67a53e1bcaa3
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-04-19 19:35:36 +0200

    Added view for adding new devices.

---
+++ Warehouse_Manage_WPF/UserInterface/Views/NewDeviceView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Warehouse_Manage_WPF.UserInterface.Views
+{
+    /// <summary>
+    /// Interaction logic for NewDeviceView.xaml
+    /// </summary>
+    public partial class NewDeviceView : UserControl
+    {
+        public NewDeviceView()
+        {
+            InitializeComponent();
+        }
+
+        private void NewProducerWindowOpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            NewProducerTitle.Visibility = Visibility.Visible;
+            NewProducerName.Visibility = Visibility.Visible;
+            NewProducerURL.Visibility = Visibility.Visible;
+            SaveNewProducer.Visibility = Visibility.Visible;
+        }
+    }
+}
